a1722ddc347ef8432d06fe5fd776e495139c2e10
FamasNdn777 <krabelodesouza52@gmail.com>
2026-04-08 02:29:38 -0300

    Delete api directory

--- api/s2/categorias.js
+++
@@ -1,6 +0,0 @@
-const { cors, categorias } = require('../_utils');
-module.exports = (req, res) => {
-    cors(res);
-    if (req.method === 'OPTIONS') return res.status(200).end();
-    res.json(categorias);
-};
